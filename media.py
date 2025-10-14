8032983fe5ffd6b3b3c7f1cf6e53a26e19e88095
unknown <goncalomiguel.marques08@gmail.com>
2025-10-14 16:56:06 +0100

    Atualizei a média

---
+++ media.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite a primeira nota : "))
+n2 = int(input("Digite a segunda nota : "))
+
+media = float((n1 + n2 )/2)
+
+if media >= 10 :
+    print ("Aluno aprovado")
+
+else:
+    print("Aluno reprovado")
+
