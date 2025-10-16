59d78e60a2c3012096e6de9d195bda096c176c78
unknown <goncalomiguel.marques08@gmail.com>
2025-10-16 11:18:35 +0100

    Exercio aula

---
+++ escola_mota.py
@@ -0,0 +1,19 @@
+print("-------------------")
+print("----Escola MIguel Torga-----")
+print("-------------------")
+
+
+n1 = int(input("Digite a primeira nota : "))
+n2 = int(input("Digite a segunda nota : "))
+
+media = float((n1 + n2 )/2)
+print("-------------------")
+print("A média é : {}" .format(media))
+
+if media >= 10 :
+    print ("Aluno aprovado")
+
+else:
+    print("Aluno reprovado")
+print("-------------------")
+    
